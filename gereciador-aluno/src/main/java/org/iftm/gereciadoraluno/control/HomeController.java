b035e6b58362fb07a3eeee39913323f13d920f62
shirley cristina mi <69362121+shirleycristinami@users.noreply.github.com>
2022-11-17 21:45:48 -0300

    Atualização 2 das classes

---
+++ gereciador-aluno/src/main/java/org/iftm/gereciadoraluno/control/HomeController.java
@@ -0,0 +1,30 @@
+package org.iftm.gereciadoraluno.control;
+
+import java.util.List;
+
+import org.iftm.gereciadoraluno.model.Aluno;
+import org.iftm.gereciadoraluno.model.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+
+	@Autowired
+	private AlunoRepository alunoRepository;
+	
+	@GetMapping("/home")
+	public String home(Model model) {
+		List<Aluno> aluno;
+		aluno = alunoRepository.findAll();
+		model.addAttribute("alunos", aluno);
+		return "home";
+	}
+
+}
